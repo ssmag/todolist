89d2553ab9890666021b4f5824ccb569a729c162
Spyros Magliveras <ss.magliveras@gmail.com>
2024-09-29 23:03:35 -0400

    Add fields to model object

--- app/src/main/java/com/example/todoapp/presentation/TodoModel.kt
+++ app/src/main/java/com/example/todoapp/presentation/TodoModel.kt
@@ -1,7 +1,12 @@
 package com.example.todoapp.presentation
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity
-class TodoModel {
-}
+data class TodoModel(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+    val title: String,
+    val desc: String
+)
